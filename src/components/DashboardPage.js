bc161eeb6534011282be831ea90a1a428ec42575
aperkaz <alainperkaz@gmail.com>
2020-04-10 15:42:24 +0200

    Refactor mock dashboard with grid css

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -23,99 +23,45 @@ class DashboardPage {
 
       <header class="columns is-multiline is-mobile is-vcentered is-centered">
         <div class="column is-8 has-text-centered">
-          <input class="input text is-info is-medium" placeholder='type dog, cat...' />
-        </div>
-
-        <div id="dashboard-page--pagination" class="column is-12 has-text-centered">
-          <nav class="pagination is-small" role="navigation" aria-label="pagination">
-            <a class="pagination-previous">Previous</a>
-            <a class="pagination-next">Next page</a>
-            <ul class="pagination-list">
-              <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
-              <li><span class="pagination-ellipsis">&hellip;</span></li>
-              <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
-              <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
-              <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
-              <li><span class="pagination-ellipsis">&hellip;</span></li>
-              <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
-            </ul>
-          </nav>
+          <input id="tag-input"class="input text is-info is-medium" placeholder='type dog, cat...' />
         </div>
       </header>
 
      <main>
-      <div class="columns is-centered">
-        <div class="column is-12">
-          <div class="columns is-gapless is-multiline is-mobile">
-    
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586394026111-22274dfcfa2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586379186050-acef54ad1a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-    
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-color: red">
-                a
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-color: green">
-                b
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-color: pink">
-                c
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-color: blue">
-                d
-              </div>
-            </div>
-    
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586394026111-22274dfcfa2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586379186050-acef54ad1a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-            <div class="column is-one-quarter">
-              <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;">
-              </div>
-            </div>
-    
-            
-    
-          </div>
-    
-        </div>
-      </div>
+        <div class="grid">
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1562887189-e5d078343de4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div> 
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586278587646-1dee11228ac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586394026111-22274dfcfa2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586379186050-acef54ad1a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586399254549-e273758e873f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586508015783-0cf09dd554ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586326448521-3f05373f8140?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          <div class="image-container" style="background-image: url(https://images.unsplash.com/photo-1586399256831-d455908a1e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80); background-repeat:no-repeat; background-position: center center; background-size: cover;"></div>
+          
+     </div>
      </main>
   </div>`;
   }
 }
 
 /*
-
+TODO: add navbar
+<div id="dashboard-page--pagination" class="column is-12 has-text-centered">
+          <nav class="pagination is-small" role="navigation" aria-label="pagination">
+            <a class="pagination-previous">Previous</a>
+            <a class="pagination-next">Next page</a>
+            <ul class="pagination-list">
+              <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+              <li><span class="pagination-ellipsis">&hellip;</span></li>
+              <li><a class="pagination-link" aria-label="Goto page 5">5</a></li>
+              <li><a class="pagination-link is-current" aria-label="Page 6" aria-current="page">6</a></li>
+              <li><a class="pagination-link" aria-label="Goto page 7">7</a></li>
+              <li><span class="pagination-ellipsis">&hellip;</span></li>
+              <li><a class="pagination-link" aria-label="Goto page 10">10</a></li>
+            </ul>
+          </nav>
+        </div>
 */
 
 module.exports = DashboardPage;
