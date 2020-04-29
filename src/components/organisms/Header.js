e582c6abac4eb4594ef882fb12266d5896dfc2d8
aperkaz <alainperkaz@gmail.com>
2020-04-29 12:35:17 +0200

    Add header organism

---
+++ src/components/organisms/Header.js
@@ -0,0 +1,129 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+
+import TagCountDisplay from "../molecules/TagCountDisplay";
+
+// TODO: style: configure theme with typography and colors
+// TODONOW: broken styles
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 1.5),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + {theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "20ch",
+  },
+}));
+
+const Header = ({
+  onInputChange,
+  onPressReset,
+  tagProcessingStatus,
+  tagCountList,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.grow}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Pacifico",
+              }}
+            >
+              taggr
+            </Typography>
+            <div className="{classes.grow}" />
+            <div className="{classes.search}">
+              <div className="{classes.searchIcon}">
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder={
+                  tagProcessingStatus ? tagProcessingStatus : "Search by tag"
+                }
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                onChange={(e) => onInputChange(e.target.value)}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+            <Button
+              style={{
+                fontFamily: "Nunito",
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+                color: "white",
+              }}
+              onClick={onPressReset}
+            >
+              Reset
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div style={{ marginTop: "8px" }}>
+        <TagCountDisplay tagCountList={tagCountList} />
+      </div>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  onInputChange: () => null,
+  onPressReset: () => null,
+  tagProcessingStatus: null,
+  tagCountList: [],
+};
+
+Header.propTypes = {
+  onInputChange: PropTypes.func,
+  onPressReset: PropTypes.func,
+  tagProcessingStatus: PropTypes.any,
+  tagCountList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      count: PropTypes.number,
+    })
+  ),
+};
+
+export default Header;
--- stories/Organism.stories.js
+++ stories/Organism.stories.js
@@ -1,21 +1,61 @@
-// import React from "react";
-// import { action } from "@storybook/addon-actions";
+import React from "react";
+import { action } from "@storybook/addon-actions";
 // import ImageGalleryComp from "../src/components/DashboardImageGallery";
-// import HeaderComp from "../src/components/DashboardHeader";
+import HeaderComp from "../src/components/organisms/Header";
 // import FullHeight from "./utils";
 // import imageList from "./mocks/imageList";
 
-// export default {
-//   title: "Organisms",
-//   components: ImageGalleryComp,
+export default {
+  title: "Organisms",
+  // components: ImageGalleryComp,
+};
+
+export const Header = () => (
+  <HeaderComp
+    onInputChange={(value) => console.log("input: ", value)}
+    onPressReset={() => console.log("reset pressed")}
+    tagCountList={[
+      { name: "dogs", count: 220 },
+      { name: "cats", count: 119 },
+      { name: "party", count: 50 },
+      { name: "suit", count: 21 },
+      { name: "beer", count: 13 },
+      { name: "pizza", count: 9 },
+      { name: "forest", count: 5 },
+      { name: "sea", count: 2 },
+      { name: "dolphin", count: 1 },
+    ]}
+  />
+);
+
+// export const HeaderProcessing = () => (
+//   <HeaderComponent
+//     onInputChange={(value) => console.log("input: ", value)}
+//     onPressReset={() => console.log("reset pressed")}
+//     tagProcessingStatus="Processing 321 / 684"
+//     tagCountList={[
+//       { name: "dogs", count: 220 },
+//       { name: "cats", count: 119 },
+//       { name: "party", count: 50 },
+//       { name: "suit", count: 21 },
+//       { name: "beer", count: 13 },
+//       { name: "pizza", count: 9 },
+//       { name: "forest", count: 5 },
+//       { name: "sea", count: 2 },
+//       { name: "dolphin", count: 1 },
+//     ]}
+//   />
+// );
+
+// export const VirtualizedGallery = () => {
+//   let imageList = require("./mocks/imageList");
+
+//   // imageList[imageList.length - 1] = { hash: "16", path: null, tags: [] };
+
+//   console.log(imageList);
+//   return (
+//     <div style={{ height: "100%" }}>
+//       <VirtualizedGalleryComponent imageList={imageList} />
+//     </div>
+//   );
 // };
-
-// export const ImageGallery = () => (
-//   <ImageGalleryComp imageList={imageList}></ImageGalleryComp>
-// );
-
-// export const DashboardHeader = () => (
-//   <FullHeight>
-//     <HeaderComp onInputChange={action("trigger search")}></HeaderComp>
-//   </FullHeight>
-// );
