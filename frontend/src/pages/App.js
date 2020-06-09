f3086680dad52654cc43ab8d31a8a0e992ff7aad
aperkaz <alainperkaz@gmail.com>
2020-06-09 14:59:42 +0200

    Add old molecules

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-logo">logo</div>
         <p>My react app</p>
         <button
           onClick={async () => {
