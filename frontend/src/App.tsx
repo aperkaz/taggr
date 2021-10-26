d2dd7ff25f3b6fbcefb312c9661b07044e37f258
alain <alainperkaz@gmail.com>
2021-10-26 15:54:09 +0200

    Clean FE styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import shared from "taggr-shared";
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           TS here!! {shared}
         </a>
       </header>
