c9f9bf5412993ccd6ea7c7fbfe40067d970ded8d
alain <alainperkaz@gmail.com>
2021-11-16 13:16:12 +0100

    Add craco to fix map bug
    https://github.com/visgl/react-map-gl/issues/1266

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -5,4 +5,4 @@ export * as types from "./types";
 /**
  * Congif variable that sets the app in development or production modes
  */
-export const IS_DEV = false;
+export const IS_DEV: boolean = true;
